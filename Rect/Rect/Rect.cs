59031e2924cc590d93d9358932b2eb8ec6a5b3c5
RD <r.j.deja@gmail.com>
2023-05-27 08:52:19 +0200

    first version

---
+++ Rect/Rect/Rect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rect
+{
+    class Rect
+    {
+        private int w, h;
+        public Rect(int w, int h)
+        {
+            this.w = w;
+            this.h = h;
+        }
+
+        public int Area()
+        {
+            return w * h;
+        }
+    }
+}
